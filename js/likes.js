8930892c05f83e3648c5a7eee5dcf81cde4e8b0c
Herr Gromov <youriy.gromov@gmail.com>
2025-03-21 19:34:56 +0100

    Add Firebase integration to store and retrieve like counts in Realtime Database

--- js/likes.js
+++ js/likes.js
@@ -15,3 +15,37 @@ hearts.forEach(function (heart) {
     heart.classList.toggle('added');
   };
 });
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9ObwmlJEsSvsH0_kQBM8z_abGIprLJr8',
+  authDomain: 'gromchat-a2482.firebaseapp.com',
+  databaseURL:
+    'https://gromchat-a2482-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'gromchat-a2482',
+  storageBucket: 'gromchat-a2482.appspot.com',
+  messagingSenderId: '397594996637',
+  appId: '1:397594996637:web:cde498dd429a737359c974',
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+document.querySelectorAll('.heart').forEach((heart, index) => {
+  let likesNumber = heart.querySelector('.likes-number');
+
+  // Получаем лайки из Firebase
+  db.ref('likes/' + index).on('value', (snapshot) => {
+    likesNumber.textContent = snapshot.val() || 0;
+  });
+
+  heart.onclick = function () {
+    let ref = db.ref('likes/' + index);
+    ref.transaction((likes) => {
+      return heart.classList.contains('added')
+        ? (likes || 0) - 1
+        : (likes || 0) + 1;
+    });
+
+    heart.classList.toggle('added');
+  };
+});
